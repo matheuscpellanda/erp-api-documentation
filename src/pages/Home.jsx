53d66d35de7ebe7d694cd1399b061af5be637cf0
matheuscpellanda <matheuscpellanda@hotmail.com>
2023-01-18 15:04:56 -0300

    Added Example of Shipping Tracking

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,40 +8,55 @@ import example03 from '../images/example_03.png';
 import example04 from '../images/example_04.png';
 import example05 from '../images/example_05.png';
 import example06 from '../images/example_06.png';
-import paymentResponse200 from '../assets/examples/payment_response_200.json';
-import paymentResponse400 from '../assets/examples/payment_response_400.json';
-import paymentResponse401 from '../assets/examples/payment_response_401.json';
-import paymentResponse405 from '../assets/examples/payment_response_405.json';
-import orderCreationResponse200 from '../assets/examples/order_creation_response_200.json';
+import example07 from '../images/example_07.png';
+import example08 from '../images/example_08.png';
+import paymentResponse from '../assets/examples/payment_response.json';
+import orderCreationResponse from '../assets/examples/order_creation_response.json';
+import shippingTrackingResponse from '../assets/examples/shipping_response.json';
 import CodeBox from '../components/CodeBox';
 
 const selectPaymentResponse = (code) => {
   switch (code) {
     case '200':
-      return paymentResponse200;
+      return paymentResponse['200'];
     case '400':
-      return paymentResponse400;
+      return paymentResponse['400'];
     case '401':
-      return paymentResponse401;
+      return paymentResponse['401'];
     case '405':
-      return paymentResponse405;
+      return paymentResponse['405'];
     default:
-      return paymentResponse200;
+      return paymentResponse['200'];
   }
 };
 
 const selectOrderCreationResponse = (code) => {
   switch (code) {
     case '200':
-      return orderCreationResponse200;
+      return orderCreationResponse['200'];
     case '400':
-      return paymentResponse400;
+      return orderCreationResponse['400'];
     case '401':
-      return paymentResponse401;
+      return orderCreationResponse['401'];
     case '405':
-      return paymentResponse405;
+      return orderCreationResponse['405'];
     default:
-      return orderCreationResponse200;
+      return orderCreationResponse['200'];
+  }
+};
+
+const selectShippingTrackingResponse = (code) => {
+  switch (code) {
+    case '200':
+      return shippingTrackingResponse['200'];
+    case '400':
+      return shippingTrackingResponse['400'];
+    case '401':
+      return shippingTrackingResponse['401'];
+    case '405':
+      return shippingTrackingResponse['405'];
+    default:
+      return shippingTrackingResponse['200'];
   }
 };
 
@@ -66,8 +81,18 @@ export default class Home extends Component {
     });
   };
 
+  showResponseShippingTracking = ({ target: { innerText } }) => {
+    this.setState({
+      showShippingTrackingResponse: innerText,
+    });
+  };
+
   render() {
-    const { showPaymentResponse, showOrderCreationResponse } = this.state;
+    const {
+      showPaymentResponse,
+      showOrderCreationResponse,
+      showShippingTrackingResponse,
+    } = this.state;
     return (
       <>
         <Header />
@@ -80,7 +105,7 @@ export default class Home extends Component {
             </h1>
             <div className="row-get-started">
               <button type="button" className="home-get-started">Get Started</button>
-              <h2 className="home-last-updated">Last updated: 04/01/2023</h2>
+              <h2 className="home-last-updated">Last Update: 18/01/2023</h2>
             </div>
           </div>
           <h2 className="title-example">Example of Order Creation</h2>
@@ -88,7 +113,6 @@ export default class Home extends Component {
             <div className="row-images">
               <img className="image-example" src={example03} alt="Exemplo código 03" />
               <img className="image-example" src={example04} alt="Exemplo código 04" />
-
               <img className="image-example" src={example05} alt="Exemplo código 05" />
               <img className="image-example" src={example06} alt="Exemplo código 06" />
             </div>
@@ -123,6 +147,25 @@ export default class Home extends Component {
               </div>
               <CodeBox
                 data={selectPaymentResponse(showPaymentResponse)}
+              />
+            </div>
+          </div>
+          <h2 className="title-example">Example of Shipping Tracking</h2>
+          <div className="row-example">
+            <div className="row-images">
+              <img className="image-example" src={example07} alt="Exemplo código 07" />
+              <img className="image-example" src={example08} alt="Exemplo código 08" />
+            </div>
+            <div className="column">
+              <div className="response-header">
+                Response Example
+                <button type="button" onClick={this.showResponseShippingTracking}>200</button>
+                <button type="button" onClick={this.showResponseShippingTracking}>400</button>
+                <button type="button" onClick={this.showResponseShippingTracking}>401</button>
+                <button type="button" onClick={this.showResponseShippingTracking}>405</button>
+              </div>
+              <CodeBox
+                data={selectShippingTrackingResponse(showShippingTrackingResponse)}
               />
             </div>
           </div>
